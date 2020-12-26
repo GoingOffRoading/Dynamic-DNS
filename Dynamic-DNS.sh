feeb20c236344dc8e3214abfe66fb3ff71aae05d
Chase Westlye <CWestlye@gmail.com>
2020-12-25 22:58:48 -0800

    updates

--- Dynamic-DNS.sh
+++ Dynamic-DNS.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-### Google Domains provides an API to update a DNS "Syntheitc record". This script
-### updates a record with the script-runner's public IP, as resolved using a DNS
-### lookup.
-###
-### Google Dynamic DNS: https://support.google.com/domains/answer/6147083
-### Synthetic Records: https://support.google.com/domains/answer/6069273
-
-# Resolve current public IP
-IP=$( dig +short myip.opendns.com @resolver1.opendns.com )
-# Update Google DNS Record
-URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${HOSTNAME}&myip=${IP}"
-curl -s $URL
-
-echo " ${HOSTNAME}"
+for each in websites/*.sh ; do bash $each ; done
